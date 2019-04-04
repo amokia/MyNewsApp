7cc3a51b662cdcc994c3a370a6421dc1c78b5d0a
amos kiarii <amokiarii@gmail.com>
2019-04-03 23:58:55 -0700

    Initial commit

---
+++ app/src/main/java/com/example/proamoo/mynewsapp/api/ApiInterface.java
@@ -0,0 +1,30 @@
+package com.example.proamoo.mynewsapp.api;
+
+
+
+import com.example.proamoo.mynewsapp.models.News;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+
+    @GET("top-headlines")
+    Call<News> getNews(
+
+            @Query("country") String country,
+            @Query("apiKey") String apiKey
+
+    );
+
+    @GET("everything")
+    Call<News> getNewsSearch(
+
+            @Query("q") String keyword,
+            @Query("language") String language,
+            @Query("sortBy") String sortBy,
+            @Query("apiKey") String apiKey
+
+    );
+
+}
